0237581332e719d4ae7ce021d533a6ed906ee237
Pascal Welsch <pascal@welsch.dev>
2019-11-07 17:28:07 +0100

    Remove meta and add more tests

--- test/all_tests.dart
+++ test/all_tests.dart
@@ -1,9 +1,11 @@
 import 'src/deep_pick_test.dart' as deep_pick_test;
 import 'src/parsing_test.dart' as parsing_test;
+import 'src/pick_test.dart' as pick_test;
 import 'src/required_pick_test.dart' as required_pick_test;
 
 void main() {
   deep_pick_test.main();
   parsing_test.main();
+  pick_test.main();
   required_pick_test.main();
 }
