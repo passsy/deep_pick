2445d565234e7f08254a590f053450c3a8c6c52a
Pascal Welsch <pascal@welsch.dev>
2019-11-09 21:44:10 +0100

    Refactor tests

--- test/src/pick_test.dart
+++ test/src/pick_test.dart
@@ -2,38 +2,200 @@ import 'package:deep_pick/deep_pick.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test("null is acceptable", () {
-    final p = pick(null);
-    expect(p.value, isNull);
+  group("Pick", () {
+    test("pick from null returns null Pick with full location", () {
+      final p = pick(null, 'some', 'path');
+      expect(p.path, ['some', 'path']);
+      expect(p.value, null);
+    });
+
+    test("toString() prints value and path", () {
+      // ignore: deprecated_member_use_from_same_package
+      expect(Pick("a", ["b", 0]).toString(), "Pick(value=a, path=[b, 0])");
+    });
+
+    test(
+        "picking from sets by index is illegal "
+        "because to order is not guaranteed", () {
+      final data = {
+        "set": {"a", "b", "c"},
+      };
+      expect(
+          () => pick(data, 'set', 0),
+          throwsA(isA<PickException>().having(
+              (e) => e.toString(),
+              'toString',
+              allOf(
+                  contains('[set]'), contains("Set"), contains("index (0)")))));
+    });
+
+    test("call()", () {
+      final data = [
+        {"name": "John Snow"},
+        {"name": "Daenerys Targaryen"},
+      ];
+
+      final picked = pick(data, 0);
+      expect(picked.value, {"name": "John Snow"});
+
+      // pick further
+      expect(picked("name").required().asString(), "John Snow");
+    });
   });
 
-  test("toString() works as expected", () {
-    // ignore: deprecated_member_use_from_same_package
-    expect(Pick("a", ["b", 0]).toString(), "Pick(value=a, path=[b, 0])");
+  group("parsing", () {
+    test("asStringOrNull()", () {
+      expect(picked("adam").asStringOrNull(), "adam");
+      expect(nullPick().asStringOrNull(), isNull);
+    });
+
+    test("asMapOrNull()", () {
+      expect(picked({"ab": "cd"}).asMapOrNull(), {"ab": "cd"});
+      expect(nullPick().asMapOrNull(), isNull);
+    });
+
+    test("asMapOrEmpty()", () {
+      expect(picked({"ab": "cd"}).asMapOrEmpty(), {"ab": "cd"});
+      expect(picked("a").asMapOrEmpty(), {});
+      expect(nullPick().asMapOrEmpty(), {});
+    });
+
+    test("asListOrNull()", () {
+      expect(picked([1, 2, 3]).asListOrNull<int>(), [1, 2, 3]);
+      expect(nullPick().asListOrNull<int>(), isNull);
+    });
+
+    test("asListOrEmpty()", () {
+      expect(picked([1, 2, 3]).asListOrEmpty<int>(), [1, 2, 3]);
+      expect(picked("a").asListOrEmpty<int>(), []);
+      expect(nullPick().asListOrEmpty<int>(), []);
+    });
+
+    test("asBoolOrNull()", () {
+      expect(picked(true).asBoolOrNull(), isTrue);
+      expect(nullPick().asBoolOrNull(), isNull);
+    });
+
+    test("asBoolOrTrue()", () {
+      expect(picked(true).asBoolOrTrue(), isTrue);
+      expect(picked(false).asBoolOrTrue(), isFalse);
+      expect(nullPick().asBoolOrTrue(), isTrue);
+    });
+
+    test("asBoolOrFalse()", () {
+      expect(picked(true).asBoolOrFalse(), isTrue);
+      expect(picked(false).asBoolOrFalse(), isFalse);
+      expect(nullPick().asBoolOrFalse(), isFalse);
+    });
+
+    test("asIntOrNull()", () {
+      expect(picked(1).asIntOrNull(), 1);
+      expect(nullPick().asIntOrNull(), isNull);
+    });
+
+    test("asDoubleOrNull()", () {
+      expect(picked(1).asDoubleOrNull(), 1.0);
+      expect(picked(2.0).asDoubleOrNull(), 2.0);
+      expect(picked("3.0").asDoubleOrNull(), 3.0);
+      expect(picked("a").asDoubleOrNull(), isNull);
+      expect(nullPick().asDoubleOrNull(), isNull);
+    });
+
+    test("asDateTimeOrNull()", () {
+      expect(picked("2012-02-27 13:27:00,123456z").asDateTimeOrNull(),
+          DateTime.utc(2012, 2, 27, 13, 27, 0, 123, 456));
+      expect(picked("1").asDateTimeOrNull(), isNull);
+      expect(picked("Bubblegum").asDateTimeOrNull(), isNull);
+      expect(nullPick().asDateTimeOrNull(), isNull);
+    });
+
+    test("letOrNull()", () {
+      expect(
+          picked({"name": "John Snow"})
+              .letOrNull((pick) => Person.fromJson(pick.asMap())),
+          Person(name: "John Snow"));
+      expect(
+          nullPick().letOrNull((pick) => Person.fromJson(pick.asMap())), null);
+    });
+
+    test("asListOrEmpty(Pick -> T)", () {
+      final data = [
+        {"name": "John Snow"},
+        {"name": "Daenerys Targaryen"},
+      ];
+      expect(picked(data).asListOrEmpty((it) => Person.fromJson(it.asMap())), [
+        Person(name: "John Snow"),
+        Person(name: "Daenerys Targaryen"),
+      ]);
+      expect(picked([]).asList((pick) => Person.fromJson(pick.asMap())), []);
+      expect(nullPick().asListOrEmpty((pick) => Person.fromJson(pick.asMap())),
+          []);
+    });
+
+    test("asListOrNull(Pick -> T)", () {
+      final data = [
+        {"name": "John Snow"},
+        {"name": "Daenerys Targaryen"},
+      ];
+      expect(
+          picked(data).asListOrNull((pick) => Person.fromJson(pick.asMap())), [
+        Person(name: "John Snow"),
+        Person(name: "Daenerys Targaryen"),
+      ]);
+      expect(
+          picked([]).asListOrNull((pick) => Person.fromJson(pick.asMap())), []);
+      expect(nullPick().asListOrNull((pick) => Person.fromJson(pick.asMap())),
+          null);
+    });
   });
 
-  test(
-      "picking from sets by index is illegal "
-      "because to order is not guaranteed", () {
-    final data = {
-      "set": {"a", "b", "c"},
-    };
-    expect(
-        () => pick(data, 'set', 0),
-        throwsA(isA<PickException>().having((e) => e.toString(), 'toString',
-            allOf(contains('[set]'), contains("Set"), contains("index (0)")))));
+  group("invalid pick", () {
+    test("out of range in list returns null pick", () {
+      final data = [
+        {"name": "John Snow"},
+        {"name": "Daenerys Targaryen"},
+      ];
+      expect(pick(data, 10).value, isNull);
+    });
+
+    test("unknown property in map returns null", () {
+      final data = {"name": "John Snow"};
+      expect(pick(data, 'birthday').value, isNull);
+    });
   });
+}
 
-  test("pick further", () {
-    final data = [
-      {"name": "John Snow"},
-      {"name": "Daenerys Targaryen"},
-    ];
+Pick picked(dynamic value) {
+  return pick([value], 0);
+}
 
-    final picked = pick(data, 0);
-    expect(picked.value, {"name": "John Snow"});
+Pick nullPick() {
+  return pick(<String, dynamic>{}, "unknownKey");
+}
 
-    // pick further
-    expect(picked("name").required().asString(), "John Snow");
-  });
+Matcher pickException({List<String> containing}) {
+  return const TypeMatcher<PickException>()
+      .having((e) => e.message, 'message', stringContainsInOrder(containing));
+}
+
+class Person {
+  Person({
+    this.name,
+  }) : assert(name != null);
+
+  factory Person.fromJson(Map<String, dynamic> data) {
+    return Person(
+      name: pick(data, "name").required().asString(),
+    );
+  }
+
+  final String name;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is Person && runtimeType == other.runtimeType && name == other.name;
+
+  @override
+  int get hashCode => name.hashCode;
 }
