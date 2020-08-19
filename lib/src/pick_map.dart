c88df56b628daea77c11458891da9b7cc41c5008
Pascal Welsch <pascal@welsch.dev>
2020-08-19 13:02:11 +0200

    Check the type when parsing maps
    Also return correct errors for asMapOr[Empty|Null]. Similar to asListOr[Empty|Null]

--- lib/src/pick_map.dart
+++ lib/src/pick_map.dart
@@ -3,7 +3,10 @@ import 'package:deep_pick/src/pick.dart';
 extension MapPick on RequiredPick {
   Map<RK, RV> asMap<RK, RV>() {
     if (value is Map) {
-      return (value as Map<dynamic, dynamic>).cast();
+      final Map<RK, RV> view = (value as Map<dynamic, dynamic>).cast();
+      // create copy of casted view so all items are type checked here
+      // and not lazily type checked when accessing them
+      return Map.of(view);
     } else {
       throw PickException(
           "value $value of type ${value.runtimeType} at location ${location()} can't be casted to Map<dynamic, dynamic>");
@@ -27,20 +30,13 @@ extension NullableMapPick on Pick {
 
   Map<RK, RV> asMapOrEmpty<RK, RV>() {
     if (value == null) return <RK, RV>{};
-
-    try {
-      return required().asMap();
-    } catch (_) {
-      return <RK, RV>{};
-    }
+    if (value is! Map) return <RK, RV>{};
+    return required().asMap();
   }
 
   Map<RK, RV> /*?*/ asMapOrNull<RK, RV>() {
     if (value == null) return null;
-    try {
-      return required().asMap();
-    } catch (_) {
-      return null;
-    }
+    if (value is! Map) return null;
+    return required().asMap();
   }
 }
