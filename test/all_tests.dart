6cc69e8e2b9d186382c79a15684b407bad68bed9
Pascal Welsch <pascal@welsch.dev>
2020-11-22 15:06:05 +0100

    Remove deprecated parseJsonTo* methods

--- test/all_tests.dart
+++ test/all_tests.dart
@@ -1,10 +1,7 @@
-import 'src/deprecated_parse_methods_test.dart'
-    as deprecated_parse_methods_test;
 import 'src/pick_test.dart' as pick_test;
 import 'src/required_pick_test.dart' as required_pick_test;
 
 void main() {
-  deprecated_parse_methods_test.main();
   pick_test.main();
   required_pick_test.main();
 }
