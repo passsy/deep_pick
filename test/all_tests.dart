530ba913e87237a0cf6b6e0fd10fe9fc07ac1a26
Pascal Welsch <pascal@welsch.dev>
2020-12-05 23:40:46 +0100

    Add new asDoubleOrThrow

--- test/all_tests.dart
+++ test/all_tests.dart
@@ -1,5 +1,6 @@
 import 'src/pick_bool_test.dart' as pick_bool_test;
 import 'src/pick_datetime_test.dart' as pick_datetime_test;
+import 'src/pick_double_test.dart' as pick_double_test;
 import 'src/pick_let_test.dart' as pick_let_test;
 import 'src/pick_test.dart' as pick_test;
 import 'src/required_pick_test.dart' as required_pick_test;
@@ -7,6 +8,7 @@ import 'src/required_pick_test.dart' as required_pick_test;
 void main() {
   pick_bool_test.main();
   pick_datetime_test.main();
+  pick_double_test.main();
   pick_let_test.main();
   pick_test.main();
   required_pick_test.main();
