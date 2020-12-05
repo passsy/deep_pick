a2f8f7e0843b0a4e1c01a5114c2d725acdfdd2da
Pascal Welsch <pascal@welsch.dev>
2020-12-05 22:39:23 +0100

    Add asDateTimeOrThrow

---
+++ test/src/pick_datetime_test.dart
@@ -0,0 +1,95 @@
+import 'package:deep_pick/deep_pick.dart';
+import 'package:test/test.dart';
+
+import 'pick_test.dart';
+
+void main() {
+  group('pick().asDateTime*', () {
+    group('asDateTimeOrThrow', () {
+      test('parse String', () {
+        expect(pick('2012-02-27 13:27:00').asDateTimeOrThrow(),
+            DateTime(2012, 2, 27, 13, 27));
+        expect(pick('2012-02-27 13:27:00.123456z').asDateTimeOrThrow(),
+            DateTime.utc(2012, 2, 27, 13, 27, 0, 123, 456));
+      });
+
+      test('parse DateTime', () {
+        final time = DateTime.utc(2012, 2, 27, 13, 27, 0, 123, 456);
+        expect(pick(time.toString()).asDateTimeOrThrow(), time);
+        expect(pick(time).asDateTimeOrThrow(), time);
+      });
+
+      test('null throws', () {
+        expect(
+          () => nullPick().asDateTimeOrThrow(),
+          throwsA(pickException(containing: [
+            'required value at location `unknownKey` is absent. Use asDateTimeOrNull() when the value may be null at some point (DateTime?).'
+          ])),
+        );
+      });
+
+      test('wrong type throws', () {
+        expect(
+          () => pick(Object()).asDateTimeOrThrow(),
+          throwsA(pickException(containing: [
+            "value Instance of 'Object' of type Object at location `<root>` can not be parsed as DateTime"
+          ])),
+        );
+      });
+    });
+
+    group('asDateTime forwards to asDateTimeOrThrow', () {
+      test('parse String', () {
+        // ignore: deprecated_member_use_from_same_package
+        expect(pick('2012-02-27 13:27:00').asDateTime(),
+            DateTime(2012, 2, 27, 13, 27));
+        // ignore: deprecated_member_use_from_same_package
+        expect(pick('2012-02-27 13:27:00.123456z').asDateTime(),
+            DateTime.utc(2012, 2, 27, 13, 27, 0, 123, 456));
+      });
+
+      test('parse DateTime', () {
+        final time = DateTime.utc(2012, 2, 27, 13, 27, 0, 123, 456);
+        // ignore: deprecated_member_use_from_same_package
+        expect(pick(time.toString()).asDateTime(), time);
+        // ignore: deprecated_member_use_from_same_package
+        expect(pick(time).asDateTime(), time);
+      });
+
+      test('null throws', () {
+        expect(
+          // ignore: deprecated_member_use_from_same_package
+          () => nullPick().asDateTime(),
+          throwsA(pickException(containing: [
+            'required value at location `unknownKey` is absent. Use asDateTimeOrNull() when the value may be null at some point (DateTime?).'
+          ])),
+        );
+      });
+
+      test('wrong type throws', () {
+        expect(
+          // ignore: deprecated_member_use_from_same_package
+          () => pick(Object()).asDateTime(),
+          throwsA(pickException(containing: [
+            "value Instance of 'Object' of type Object at location `<root>` can not be parsed as DateTime"
+          ])),
+        );
+      });
+    });
+
+    group('asDateTimeOrNull', () {
+      test('parse String', () {
+        expect(pick('2012-02-27 13:27:00').asDateTimeOrNull(),
+            DateTime(2012, 2, 27, 13, 27));
+      });
+
+      test('null returns null', () {
+        expect(nullPick().asDateTimeOrNull(), isNull);
+      });
+
+      test('wrong type returns null', () {
+        expect(pick(Object()).asDateTimeOrNull(), isNull);
+      });
+    });
+  });
+}
