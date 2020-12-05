8ec4fcb1485aa00ae7da61265c06a0f1ef309576
Pascal Welsch <pascal@welsch.dev>
2020-12-05 23:57:28 +0100

    Add asIntOrThrow

--- test/all_tests.dart
+++ test/all_tests.dart
@@ -1,6 +1,7 @@
 import 'src/pick_bool_test.dart' as pick_bool_test;
 import 'src/pick_datetime_test.dart' as pick_datetime_test;
 import 'src/pick_double_test.dart' as pick_double_test;
+import 'src/pick_int_test.dart' as pick_int_test;
 import 'src/pick_let_test.dart' as pick_let_test;
 import 'src/pick_test.dart' as pick_test;
 import 'src/required_pick_test.dart' as required_pick_test;
@@ -9,6 +10,7 @@ void main() {
   pick_bool_test.main();
   pick_datetime_test.main();
   pick_double_test.main();
+  pick_int_test.main();
   pick_let_test.main();
   pick_test.main();
   required_pick_test.main();
