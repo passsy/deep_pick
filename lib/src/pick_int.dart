0bde254bfb0c589e3bf6d6f83c2ef24be80f64e6
Steven Dz <Steven.Dzionara@gmail.com>
2021-06-02 12:13:47 +0200

    Implement roundDoubleToInt property for asIntOrThrow() and asIntOrNull() (#37)
    Co-authored-by: Pascal Welsch <pascal@welsch.dev>

--- lib/src/pick_int.dart
+++ lib/src/pick_int.dart
@@ -8,12 +8,25 @@ extension NullableIntPick on Pick {
   ///
   /// {@template Pick.asInt}
   /// Parses the picked value as [int]. Also tries to parse [String] as [int]
+  /// Set [roundDouble] to round [double] to [int]
+  /// Set [truncateDouble] to cut off decimals
+  /// [roundDouble] and [truncateDouble] can not be true at the same time
   /// via [int.tryParse]
   /// {@endtemplate}
-  int _parse() {
+  int _parse(bool roundDouble, bool truncateDouble) {
     final value = required().value;
+    if (roundDouble && truncateDouble) {
+      throw PickException(
+          '[roundDouble] and [truncateDouble] can not be true at the same time');
+    }
     if (value is int) {
       return value;
+    }
+    if (value is num && roundDouble) {
+      return value.round();
+    }
+    if (value is num && truncateDouble) {
+      return value.toInt();
     }
     if (value is String) {
       final parsed = int.tryParse(value);
@@ -21,27 +34,28 @@ extension NullableIntPick on Pick {
         return parsed;
       }
     }
+
     throw PickException(
-        'Type ${value.runtimeType} of $debugParsingExit can not be parsed as int');
+        'Type ${value.runtimeType} of $debugParsingExit can not be parsed as int, set [roundDouble] or [truncateDouble] to parse from double');
   }
 
   /// Returns the picked [value] as [int] or throws
   ///
   /// {@macro Pick.asInt}
-  int asIntOrThrow() {
+  int asIntOrThrow({bool roundDouble = false, bool truncateDouble = false}) {
     withContext(requiredPickErrorHintKey,
         'Use asIntOrNull() when the value may be null/absent at some point (int?).');
-    return _parse();
+    return _parse(roundDouble, truncateDouble);
   }
 
   /// Returns the picked [value] as [int?] or returns `null` when the picked
   /// value is absent
   ///
   /// {@macro Pick.asInt}
-  int? asIntOrNull() {
+  int? asIntOrNull({bool roundDouble = false, bool truncateDouble = false}) {
     if (value == null) return null;
     try {
-      return _parse();
+      return _parse(roundDouble, truncateDouble);
     } catch (_) {
       return null;
     }
