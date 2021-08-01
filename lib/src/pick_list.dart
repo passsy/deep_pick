913297220b71143485c0e202cc6a580b38f40d04
Pascal Welsch <pascal@welsch.dev>
2021-08-01 14:04:00 +0200

    Document asList*() methods
    Fixes #28

--- lib/src/pick_list.dart
+++ lib/src/pick_list.dart
@@ -9,6 +9,45 @@ extension NullableListPick on Pick {
     }, whenNull: (it) => it.value as T);
   }
 
+  /// Returns the items of the [List] as list, mapped each item with [map]
+  ///
+  /// {@template Pick.asList}
+  /// Each item in the list gets mapped, even when the list contains `null`
+  /// values. To simplify the api, only non-null values get mapped with the
+  /// [map] function. By default, `null` values are ignored. To explicitly
+  /// map `null` values, use the [whenNull] mapping function.
+  ///
+  /// ```dart
+  /// final persons = pick([
+  ///   {'name': 'John Snow'},
+  ///   {'name': 'Daenerys Targaryen'},
+  ///   null, // <-- valid value
+  /// ]).asListOrThrow(
+  ///   (pick) => Person.fromPick(pick),
+  ///   whenNull: (it) => null,
+  /// )
+  ///
+  /// // persons
+  /// [
+  ///   Person(name: 'John Snow'),
+  ///   Person(name: 'Daenerys Targaryen'),
+  ///   null,
+  /// ]
+  /// ```
+  ///
+  /// For some apis it is important to get the access to the [index] of an
+  /// element in the list. Access it via [index] which is only available for
+  /// list elements, otherwise `null`.
+  ///
+  /// Usage:
+  ///
+  /// ```dart
+  /// pick(["John", "Paul", "George", "Ringo"]).asListOrThrow((pick) {
+  ///  final index = pick.index!;
+  ///  return Artist(id: index, name: pick.asStringOrThrow());
+  /// );
+  /// ```
+  /// {@endtemplate}
   List<T> _parse<T>(T Function(RequiredPick) map,
       {T Function(Pick pick)? whenNull}) {
     final value = required().value;
@@ -44,6 +83,10 @@ extension NullableListPick on Pick {
         'Type ${value.runtimeType} of $debugParsingExit can not be casted to List<dynamic>');
   }
 
+  /// Returns the picked [value] as [List]. This method throws when [value] is
+  /// not a `List` or [isAbsent]
+  ///
+  /// {@macro Pick.asList}
   List<T> asListOrThrow<T>(T Function(RequiredPick) map,
       {T Function(Pick pick)? whenNull}) {
     withContext(requiredPickErrorHintKey,
@@ -51,6 +94,10 @@ extension NullableListPick on Pick {
     return _parse(map, whenNull: whenNull);
   }
 
+  /// Returns the picked [value] as [List] or an empty list when the `value`
+  /// isn't a [List] or [isAbsent].
+  ///
+  /// {@macro Pick.asList}
   List<T> asListOrEmpty<T>(T Function(RequiredPick) map,
       {T Function(Pick pick)? whenNull}) {
     if (value == null) return <T>[];
@@ -58,6 +105,10 @@ extension NullableListPick on Pick {
     return _parse(map, whenNull: whenNull);
   }
 
+  /// Returns the picked [value] as [List] or null when the `value`
+  /// isn't a [List] or [isAbsent].
+  ///
+  /// {@macro Pick.asList}
   List<T>? asListOrNull<T>(T Function(RequiredPick) map,
       {T Function(Pick pick)? whenNull}) {
     if (value == null) return null;
