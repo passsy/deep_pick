1bc4ce8ed7b260c41d6a15a919bc665036587ef6
Pascal Welsch <pascal@welsch.dev>
2022-11-03 20:22:40 +0100

    Fix coverage script

--- tool/run_coverage_locally.sh
+++ tool/run_coverage_locally.sh
@@ -1,33 +1,19 @@
 #!/bin/sh
+set -e
 
-(pub global list | grep coverage) || {
+(dart pub global list | grep coverage) || {
   # install coverage when not found
-  pub global activate coverage
+  dart pub global activate coverage
 }
-
-pub global run coverage:collect_coverage \
-    --port=8111 \
-    --out=out/coverage/coverage.json \
-    --wait-paused \
-    --resume-isolates \
-    &
-
-dart \
-    --disable-service-auth-codes \
-    --enable-vm-service=8111 \
-    --pause-isolates-on-exit  \
-    --enable-asserts \
-    test/all_tests.dart
-
-pub global run coverage:format_coverage \
+dart pub global run coverage:test_with_coverage
+dart pub global run coverage:format_coverage \
+    --packages=.dart_tool/package_config.json \
     --lcov \
-    --in=out/coverage/coverage.json \
-    --out=out/coverage/lcov.info \
-    --packages=.packages \
-    --report-on lib
+    -i coverage/coverage.json \
+    -o coverage/lcov.info
 
 if type genhtml >/dev/null 2>&1; then
- genhtml -o out/coverage/html out/coverage/lcov.info
+ genhtml -o coverage/html coverage/lcov.info
  echo "open coverage report $PWD/out/coverage/html/index.html"
 else
  echo "genhtml not installed, can't generate html coverage output"
